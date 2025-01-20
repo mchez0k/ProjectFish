cc8a06a516a34ee9374ae6aed9e953802c0e1e0c
mchez0k <mchezok@gmail.com>
2025-01-20 22:41:16 +0300

    Подчистил код, удалил лишние пакеты

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using ProjectFish.Movement;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour, IMovement
